28914ba80df8d640334860875ae6df53d46eefae
Eric Mpapa <eric@pop-os.localdomain>
2024-01-04 17:18:04 +0200

    update

--- bin/gestion_caisse.dart
+++ bin/gestion_caisse.dart
@@ -1,5 +1,51 @@
-import 'package:gestion_caisse/livre_caisse.dart' as gestion_caisse;
+import 'dart:io';
+
+import 'package:gestion_caisse/enregistrement.dart';
+import 'package:gestion_caisse/livre_caisse.dart';
+
+bool continuerOuQuitter() {
+  var ret = false;
+  return ret;
+}
+
+bool nouvelleEntree(LivreCaisse livreCaisse) {
+  var ret = false;
+  stdout.write("Saisissez le libellé: ");
+  var libelle = stdin.readLineSync()!;
+  stdout.write("Saisissez le montant: ");
+  var montant = double.parse(stdin.readLineSync()!);
+  livreCaisse.ajouterEnregitrement(Enregistrement(
+      numero: LivreCaisse.seq, libelle: libelle, credit: montant));
+
+  return ret;
+}
+
+bool menu() {
+  bool ret = false;
+  print("Saisissez:");
+  print("[1]. Pour enregistrer une nouvelle entrée");
+  print("[2]. Pour enregistrer une nouvelle sortie");
+  print("[3]. Pour afficher tous les enregistrements");
+  print("[4]. Pour afficher les enregistrements sur une période donnée");
+  print("[Toute autre touche]. Pour quitter");
+
+  var choix = int.parse(stdin.readLineSync()!);
+
+  switch (choix) {
+    case 1:
+      break;
+    case 2:
+      break;
+    case 3:
+      break;
+  }
+
+  return ret;
+}
 
 void main(List<String> arguments) {
-  print('Hello world: ${gestion_caisse.calculate()}!');
+  var execution = true;
+  while (execution) {
+    execution = menu();
+  }
 }
--- lib/livre_caisse.dart
+++ lib/livre_caisse.dart
@@ -2,12 +2,34 @@ import 'package:gestion_caisse/enregistrement.dart';
 
 class LivreCaisse {
   int numeroCompte;
+  static int seq = 0;
   final _enregistrements = <Enregistrement>[];
 
   LivreCaisse({this.numeroCompte = 0});
 
   void ajouterEnregitrement(Enregistrement enregistrement) {
     _enregistrements.add(enregistrement);
+    seq++;
+  }
+
+  void afficherEnregistrements() {
+    print('LISTE DES ENREGISTREMENTS');
+    for (var enregistrement in _enregistrements) {
+      print(enregistrement);
+    }
+    print('SOLDE: ${soldeGeneral()}');
+  }
+
+  void afficherEnregistrementsParPeriode(DateTime debut, DateTime fin) {
+    print(
+        'ENREGISTREMENTS DU ${debut.day - debut.month - debut.year} au ${fin.day - fin.month - fin.year}');
+    for (var enregistrement in _enregistrements) {
+      if (enregistrement.dateEnreg.compareTo(debut) >= 0 &&
+          enregistrement.dateEnreg.compareTo(fin) <= 0) {
+        print(enregistrement);
+      }
+    }
+    print('SOLDE DE LA PERIODE: ${soldePeriode(debut, fin)}');
   }
 
   double soldeGeneral() {
